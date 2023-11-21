216fff4c41876b939cc4e2a5b61a9d832b66ba16
John Z <johnzak80@gmail.com>
2023-11-21 17:10:11 +0000

    Revert "updated models for merge"

--- api/models/buyers.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from bson.objectid import ObjectId
-from pydantic import BaseModel
-
-
-class Buyer(BaseModel):
-    user_id: ObjectId
-    orders: List[ObjectId] = []  # orders
-    comments: List[ObjectId] = []  # comments

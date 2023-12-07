5134100bbbbde932a742e93ef067b7194547ca92
John Z <johnzak80@gmail.com>
2023-12-07 15:45:12 +0000

    frontend deploy tests

--- api/main.py
+++ api/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
+    allow_origins=[os.environ.get("CORS_HOST", "*")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

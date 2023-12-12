98e8f28f0178ae2a7505b0818471ebfc1810b5de
John Z <johnzak80@gmail.com>
2023-12-12 15:14:04 +0000

    Update origin to addess cors error

--- api/main.py
+++ api/main.py
@@ -13,7 +13,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://galvanize9283031.gitlab.io/",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
